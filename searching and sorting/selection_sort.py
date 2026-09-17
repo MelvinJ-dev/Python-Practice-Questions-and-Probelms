4620f22035b3023c99396c291f37ab5e712956a0
Melvin J <jmelvin1404@gmail.com>
2026-09-17 20:30:13 +0530

    it better to try again the selection sort

--- searching and sorting/selection_sort.py
+++ searching and sorting/selection_sort.py
@@ -6,4 +6,13 @@
 
 # [12,25,11,34,90,22]
 
- 
+def selection_sort(lst):
+    length = len(lst)
+    for i in range(length):
+        min_index = i                       # 0
+        for j in range(i+1,length):
+            if(lst[j]<lst[min_index]):
+                min_index = j
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    print(lst)
+selection_sort([12,25,11,34,90,22])  
