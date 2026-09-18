f4f5c09e60a2a2734db5a66b7e9216d742e41ec9
Melvin J <jmelvin1404@gmail.com>
2026-09-18 06:01:36 +0530

    added new folder Tools and seen selction sort 'i+1' is very important in selection sort

--- searching and sorting/selection_sort.py
+++ searching and sorting/selection_sort.py
@@ -6,13 +6,27 @@
 
 # [12,25,11,34,90,22]
 
-def selection_sort(lst):
-    length = len(lst)
+# def selection_sort(lst):
+#     length = len(lst)
+#     for i in range(length):
+#         min_index = i                       # 0
+#         for j in range(i+1,length):
+#             if(lst[j]<lst[min_index]):
+#                 min_index = j
+#         lst[i], lst[min_index] = lst[min_index], lst[i]
+#     print(lst)
+# selection_sort([12,25,11,34,90,22])  
+
+# i+1 is important in selection sort
+def selection_sort(arr):
+    length = len(arr)
+
     for i in range(length):
-        min_index = i                       # 0
+        min_index = i
         for j in range(i+1,length):
-            if(lst[j]<lst[min_index]):
+            if(arr[j]<arr[min_index]):
                 min_index = j
-        lst[i], lst[min_index] = lst[min_index], lst[i]
-    print(lst)
-selection_sort([12,25,11,34,90,22])  
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+    print(arr)
+
+selection_sort([12,25,11,34,90,22])
